e49189f52fb60dbedf7b4584976dba521f7338c0
Takamichi Hori <takamichihori9@gmail.com>
2026-09-02 11:50:06 +0900

    modified s3

--- apps/api/src/s3.ts
+++ apps/api/src/s3.ts
@@ -3,7 +3,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const s3 = new S3Client({
     
-    region: "ap-northeast-1"
+    region: "ap-northeast-3"
 });
 
 const command = new PutObjectCommand({
